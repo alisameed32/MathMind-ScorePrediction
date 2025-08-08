57dd580bacbe28d012f8e4119f24fe20458d2bc5
Ali Sameed <alisameed.cs@gmail.com>
2025-08-08 12:12:41 +0500

    data ingestion module

--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,24 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(os.path.dirname(logs_path),exist_ok=True)
+# Create timestamp
+TIMESTAMP = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+
+# Get project root (one level up from where this file is located)
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Path to the folder for this run's logs
+run_log_dir = os.path.join(ROOT_DIR, "logs", TIMESTAMP)
+os.makedirs(run_log_dir, exist_ok=True)
+
+# Full path to the log file inside that folder
+LOG_FILE_PATH = os.path.join(run_log_dir, f"{TIMESTAMP}.log")
+
 
 logging.basicConfig(
-    filename=logs_path,
-    format="%(asctime)s %(lineno)d %(name)s %(levelname)s %(message)s",
-    level=logging.INFO
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] line %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
 
-if __name__ == "__main__":
-    logging.info("Logging has started.")
+
